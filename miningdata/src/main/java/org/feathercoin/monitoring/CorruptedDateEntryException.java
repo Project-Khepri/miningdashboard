50009fa51589944f8192ee8635be85fe380cd0e5
sradics <radics.sebastian@gmail.com>
2014-01-14 00:34:57 +0100

    Correct constructor

--- miningdata/src/main/java/org/feathercoin/monitoring/CorruptedDateEntryException.java
+++ miningdata/src/main/java/org/feathercoin/monitoring/CorruptedDateEntryException.java
@@ -15,8 +15,4 @@ public class CorruptedDateEntryException extends RuntimeException {
     public CorruptedDateEntryException(Throwable cause) {
         super(cause);
     }
-
-    public CorruptedDateEntryException(String message, Throwable cause, boolean enableSuppression, boolean writableStackTrace) {
-        super(message, cause, enableSuppression, writableStackTrace);
-    }
 }
